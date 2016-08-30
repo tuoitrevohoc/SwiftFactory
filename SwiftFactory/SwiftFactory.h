ea5e39061e15c97718236c7d808f6623a5fdbdef
tuoitrevohoc <tuoitrevohoc@me.com>
2016-08-30 18:25:57 +0700

    Initial Commit

---
+++ SwiftFactory/SwiftFactory.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFactory.h
+//  SwiftFactory
+//
+//  Created by Tran Thien Khiem on 8/30/16.
+//  Copyright © 2016 Tran Thien Khiem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFactory.
+FOUNDATION_EXPORT double SwiftFactoryVersionNumber;
+
+//! Project version string for SwiftFactory.
+FOUNDATION_EXPORT const unsigned char SwiftFactoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFactory/PublicHeader.h>
+
+
